490c58aaa99271bd413cffe5b0b14acab80b6632
Abdelrahman Atef <man1207468@gmail.com>
2026-06-06 21:33:02 +0300

    Enhance language management and preloader handling in admin interface

--- DAKKN.MVC/Controllers/TranslationController.cs
+++ DAKKN.MVC/Controllers/TranslationController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace DAKKN.MVC.Controllers
 {
@@ -8,10 +9,20 @@ namespace DAKKN.MVC.Controllers
         [HttpGet]
         public IActionResult SetLanguage(string culture, string returnUrl)
         {
+            if (string.IsNullOrEmpty(culture)) culture = "ar";
+            if (string.IsNullOrEmpty(returnUrl)) returnUrl = "/";
+
+            // Set the ASP.NET Core Culture cookie
             Response.Cookies.Append(
                 CookieRequestCultureProvider.DefaultCookieName,
                 CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
-                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                new CookieOptions { 
+                    Expires = DateTimeOffset.UtcNow.AddYears(1),
+                    HttpOnly = false,
+                    Secure = Request.IsHttps, // Only secure if using HTTPS
+                    SameSite = SameSiteMode.Lax,
+                    Path = "/"
+                }
             );
 
             return LocalRedirect(returnUrl);
