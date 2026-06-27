8148ce65bb18a4fb3eb451e93430489d614cf9bf
Mohamed Saber <mmms77990@gmail.com>
2026-06-27 19:45:39 +0300

    savechanges

---
+++ DAKKN.Persistence/Migrations/20260627153703_RemovePrimaryColorFromUserSettings.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAKKN.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePrimaryColorFromUserSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PrimaryColor",
+                schema: "dbo",
+                table: "UserSettings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PrimaryColor",
+                schema: "dbo",
+                table: "UserSettings",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "#3B82F6");
+        }
+    }
+}
